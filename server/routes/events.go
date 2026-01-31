7bd8e9ecee3d79d56d13a8484c1af21470d2630d
Razboy20 <razboy20@gmail.com>
2026-01-30 21:08:56 -0600

    feat(import): import timeful event from remote instance via URL

--- server/routes/events.go
+++ server/routes/events.go
@@ -3,9 +3,12 @@ package routes
 
 import (
 	"context"
-	"encoding/json" //TODO: remove this before committing
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/gin-contrib/sessions"
@@ -28,6 +31,7 @@ func InitEvents(router *gin.RouterGroup) {
 	eventRouter := router.Group("/events")
 
 	eventRouter.POST("", createEvent)
+	eventRouter.POST("/import", middleware.AuthRequired(), importEvent)
 	eventRouter.PUT("/:eventId", editEvent)
 	eventRouter.GET("/:eventId/ids", getEventIds)
 	eventRouter.GET("/:eventId", getEvent)
@@ -1647,6 +1651,181 @@ func archiveEvent(c *gin.Context) {
 	}
 
 	c.Status(http.StatusOK)
+}
+
+// @Summary Import a Timeful event from a remote instance
+// @Tags events
+// @Accept json
+// @Produce json
+// @Param payload body object{url=string} true "Object containing the URL of the remote event"
+// @Success 201 {object} object{eventId=string,shortId=string}
+// @Router /events/import [post]
+func importEvent(c *gin.Context) {
+	payload := struct {
+		URL string `json:"url" binding:"required"`
+	}{}
+	if err := c.Bind(&payload); err != nil {
+		return
+	}
+
+	userInterface, _ := c.Get("authUser")
+	user := userInterface.(*models.User)
+
+	// Parse the URL to extract base URL and event ID
+	parsed, err := url.Parse(payload.URL)
+	if err != nil || (parsed.Scheme != "http" && parsed.Scheme != "https") {
+		c.JSON(http.StatusBadRequest, responses.Error{Error: "invalid-url"})
+		return
+	}
+
+	// Extract event ID from path (e.g., /e/abc123 or /g/abc123)
+	pathParts := strings.Split(strings.Trim(parsed.Path, "/"), "/")
+	if len(pathParts) < 2 || (pathParts[0] != "e" && pathParts[0] != "g") {
+		c.JSON(http.StatusBadRequest, responses.Error{Error: "invalid-url"})
+		return
+	}
+	remoteEventId := pathParts[1]
+	baseURL := fmt.Sprintf("%s://%s", parsed.Scheme, parsed.Host)
+
+	httpClient := &http.Client{Timeout: 10 * time.Second}
+
+	// Fetch the remote event
+	eventResp, err := httpClient.Get(fmt.Sprintf("%s/api/events/%s", baseURL, remoteEventId))
+	if err != nil {
+		c.JSON(http.StatusBadGateway, responses.Error{Error: "remote-fetch-failed"})
+		return
+	}
+	defer eventResp.Body.Close()
+
+	if eventResp.StatusCode != http.StatusOK {
+		c.JSON(http.StatusBadGateway, responses.Error{Error: "remote-event-not-found"})
+		return
+	}
+
+	eventBody, err := io.ReadAll(eventResp.Body)
+	if err != nil {
+		c.JSON(http.StatusBadGateway, responses.Error{Error: "remote-fetch-failed"})
+		return
+	}
+
+	var remoteEvent models.Event
+	if err := json.Unmarshal(eventBody, &remoteEvent); err != nil {
+		c.JSON(http.StatusBadGateway, responses.Error{Error: "remote-fetch-failed"})
+		return
+	}
+
+	// Build a name lookup from the event's responses map (remote authenticated users)
+	remoteNameMap := make(map[string]string)
+	for key, resp := range remoteEvent.ResponsesMap {
+		if resp != nil && resp.User != nil && resp.User.FirstName != "" {
+			remoteNameMap[key] = resp.User.FirstName
+		}
+	}
+
+	// Fetch remote responses with availability data
+	var timeMin, timeMax time.Time
+	for i, d := range remoteEvent.Dates {
+		t := d.Time()
+		if i == 0 || t.Before(timeMin) {
+			timeMin = t
+		}
+		if i == 0 || t.After(timeMax) {
+			timeMax = t
+		}
+	}
+	// Extend timeMax by 1 day to cover the full range
+	timeMax = timeMax.AddDate(0, 0, 1)
+
+	responsesURL := fmt.Sprintf("%s/api/events/%s/responses?timeMin=%s&timeMax=%s",
+		baseURL, remoteEventId,
+		url.QueryEscape(timeMin.Format(time.RFC3339)),
+		url.QueryEscape(timeMax.Format(time.RFC3339)),
+	)
+	respResp, err := httpClient.Get(responsesURL)
+	if err != nil {
+		c.JSON(http.StatusBadGateway, responses.Error{Error: "remote-fetch-failed"})
+		return
+	}
+	defer respResp.Body.Close()
+
+	respBody, err := io.ReadAll(respResp.Body)
+	if err != nil {
+		c.JSON(http.StatusBadGateway, responses.Error{Error: "remote-fetch-failed"})
+		return
+	}
+
+	remoteResponses := make(map[string]*models.Response)
+	if respResp.StatusCode == http.StatusOK {
+		if err := json.Unmarshal(respBody, &remoteResponses); err != nil {
+			c.JSON(http.StatusBadGateway, responses.Error{Error: "remote-fetch-failed"})
+			return
+		}
+	}
+
+	// Create local event with new identity
+	newId := primitive.NewObjectID()
+	shortId := db.GenerateShortEventId(newId)
+	numResponses := 0
+
+	remoteEvent.Id = newId
+	remoteEvent.OwnerId = user.Id
+	remoteEvent.ShortId = &shortId
+	remoteEvent.NumResponses = &numResponses
+	remoteEvent.Remindees = nil
+	remoteEvent.Attendees = nil
+	remoteEvent.ResponsesMap = nil
+	remoteEvent.When2meetHref = nil
+	remoteEvent.ScheduledEvent = nil
+	remoteEvent.CalendarEventId = ""
+	remoteEvent.CreatorPosthogId = nil
+	remoteEvent.SignUpResponses = make(map[string]*models.SignUpResponse)
+
+	_, err = db.EventsCollection.InsertOne(context.Background(), remoteEvent)
+	if err != nil {
+		logger.StdErr.Panicln(err)
+	}
+
+	// Import responses as guest entries
+	for key, resp := range remoteResponses {
+		name := resp.Name
+		if name == "" {
+			if n, ok := remoteNameMap[key]; ok {
+				name = n
+			} else {
+				name = key
+			}
+		}
+
+		eventResponse := models.EventResponse{
+			Id:      primitive.NewObjectID(),
+			EventId: newId,
+			UserId:  name,
+			Response: &models.Response{
+				Name:               name,
+				Availability:       resp.Availability,
+				IfNeeded:           resp.IfNeeded,
+				ManualAvailability: resp.ManualAvailability,
+			},
+		}
+
+		_, err := db.EventResponsesCollection.InsertOne(context.Background(), eventResponse)
+		if err != nil {
+			logger.StdErr.Panicln(err)
+		}
+		*remoteEvent.NumResponses++
+	}
+
+	// Update NumResponses on the event
+	db.EventsCollection.UpdateOne(context.Background(),
+		bson.M{"_id": newId},
+		bson.M{"$set": bson.M{"numResponses": remoteEvent.NumResponses}},
+	)
+
+	// Increment user's NumEventsCreated
+	user.NumEventsCreated++
+	db.UsersCollection.UpdateOne(context.Background(), bson.M{"_id": user.Id}, bson.M{"$set": user})
+
+	c.JSON(http.StatusCreated, gin.H{"eventId": newId.Hex(), "shortId": shortId})
 }
 
 // Helper function to find a response by userId
