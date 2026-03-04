d06524af1171c1257134f5a2b6253c202e8ef281
jonathan liu <liu.z.jonathan@gmail.com>
2026-03-04 01:41:07 -0500

    center ads

--- frontend/src/components/event/PubliftAd.vue
+++ frontend/src/components/event/PubliftAd.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="showAd"
-    class="tw-relative tw-rounded-lg tw-bg-light-gray tw-p-3 tw-pt-5"
+    class="tw-relative tw-flex tw-items-center tw-justify-center tw-rounded-lg tw-bg-light-gray tw-p-3 tw-pt-5"
   >
     <span
       class="tw-absolute tw-left-1/2 tw-top-0 tw--translate-x-1/2 tw--translate-y-1/2 tw-rounded-full tw-border tw-border-light-gray-stroke tw-bg-off-white tw-px-2.5 tw-py-0.5 tw-text-[10px] tw-font-medium tw-uppercase tw-tracking-wide tw-text-dark-gray"
