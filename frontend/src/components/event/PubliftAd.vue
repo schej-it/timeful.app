18af6a0ee1eeb5f44b9fcd6e5af66f865bef87bd
jonathan liu <liu.z.jonathan@gmail.com>
2026-03-08 18:04:05 -0400

    remove ads button

--- frontend/src/components/event/PubliftAd.vue
+++ frontend/src/components/event/PubliftAd.vue
@@ -4,9 +4,18 @@
     class="tw-relative tw-rounded-lg tw-bg-light-gray tw-p-3 tw-pt-5"
   >
     <span
-      class="tw-absolute tw-left-1/2 tw-top-0 tw--translate-x-1/2 tw--translate-y-1/2 tw-rounded-full tw-border tw-border-light-gray-stroke tw-bg-off-white tw-px-2.5 tw-py-0.5 tw-text-[10px] tw-font-medium tw-uppercase tw-tracking-wide tw-text-dark-gray"
+      class="tw-absolute tw-left-1/2 tw-top-0 tw-flex tw-w-[90%] tw--translate-x-1/2 tw--translate-y-1/2 tw-justify-center tw-gap-x-1 tw-rounded-full tw-border tw-border-light-gray-stroke tw-bg-off-white tw-px-2.5 tw-py-0.5"
     >
-      advertisement
+      <div
+        class="tw-text-[10px] tw-font-medium tw-uppercase tw-tracking-wide tw-text-dark-gray"
+      >
+        advertisement
+      </div>
+      <div
+        class="tw-cursor-pointer tw-select-none tw-text-center tw-text-[10px] tw-font-medium tw-text-blue tw-underline"
+      >
+        Remove ads
+      </div>
     </span>
     <slot />
   </div>
