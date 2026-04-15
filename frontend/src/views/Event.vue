8639971f3efdd5d364460184043e9d2d02375639
jonathan liu <liu.z.jonathan@gmail.com>
2026-04-15 00:11:51 -0400

    don't show video on mobile

--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -3,8 +3,8 @@
     <FormerlyKnownAs
       class="tw-mx-auto tw-mb-10 tw-mt-3 tw-max-w-6xl tw-pl-4 sm:tw-pl-12"
     />
-    <!-- Video Ad -->
-    <div ref="videoAdContainer"></div>
+      <!-- Video Ad (desktop only) -->
+      <div v-if="!isPhone" ref="videoAdContainer"></div>
     <div v-if="event" class="tw-mt-8 tw-h-full">
       <!-- Mark availability option dialog -->
       <MarkAvailabilityDialog
@@ -703,7 +703,7 @@ export default {
     ...mapMutations(["setAuthUser"]),
 
     loadVideoAd() {
-      if (this.$refs.videoAdContainer) {
+      if (!this.isPhone && this.$refs.videoAdContainer) {
         const script = document.createElement("script")
         script.type = "text/javascript"
         script.src =
